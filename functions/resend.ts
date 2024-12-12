36fa34a25ec8f346d201602832e8f7278e86bb53
AbhiraajV <abhiraajverma@gmail.com>
2024-12-12 12:49:45 +0530

    resend api

--- functions/resend.ts
+++ functions/resend.ts
@@ -1,5 +1,6 @@
 import { Resend } from 'resend';
 const k = process.env.RESEND_API_KEY!
+const baseurl = process.env.FRONTEND
 const resend = new Resend(k);
 import prisma from '../prisma';
 export async function sendMail(userId:string,reportId:string) {
@@ -15,7 +16,7 @@ export async function sendMail(userId:string,reportId:string) {
         <div style="font-family: Arial, sans-serif; text-align: center; padding: 20px;">
     <strong>It works!</strong>
     <p>Your SaaS report is ready! Click the button below to view it:</p>
-    <a href="http://localhost:3000/report/${reportId}" 
+    <a href="${baseurl}${reportId}" 
        style="display: inline-block; padding: 10px 20px; margin-top: 10px; font-size: 16px; color: #ffffff; background-color: #007BFF; text-decoration: none; border-radius: 5px;">
         View Your Report
     </a>
